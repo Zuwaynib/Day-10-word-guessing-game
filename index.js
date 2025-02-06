413599090c789ab67ce0cac6095faa51212797ae
Zuwaynib <zainabadeoje@gmail.com>
2025-02-06 23:38:07 +0100

    pushing all project files

---
+++ index.js
@@ -0,0 +1,5 @@
+function generateWords() {
+    let random = Math.floor(Math.random * wordList.word.length);
+    console.log(random);
+}
+generateWords();
---
+++ wordlist.js
@@ -0,0 +1,50 @@
+const wordList = [
+    {
+        word: "python",
+        hint: "A programming language"
+    },
+    {
+        word: "laptop",
+        hint: "A personal computer"
+    },
+    {
+        word: "tired",
+        hint: "Another word for fatigue"
+    },
+    {
+        word: "trumpet",
+        hint: "A musical instrument"
+    },
+    {
+        word: "diamond",
+        hint: "An isotope of carbon"
+    },
+    {
+        word: "snake",
+        hint: "A poisonous animal"
+    },
+    {
+        word: "dad",
+        hint: "Another word for father"
+    },
+    {
+        word: "jovial",
+        hint: "Cheerful and friendly"
+    },
+    {
+        word: "brim",
+        hint: "The top edge of a container"
+    },
+    {
+        word: "dwell",
+        hint: "To focus on something"
+    },
+    {
+        word: "tiktok",
+        hint: "A popular app"
+    },
+    {
+        word: "lush",
+        hint: "Rich, abundant, luxurious"
+    }
+]
